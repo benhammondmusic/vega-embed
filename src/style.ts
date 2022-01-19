52bd3be2034862487443629d09b02a987d812a86
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-18 23:07:48 -0700

    b w ring

--- src/style.ts
+++ src/style.ts
@@ -34,6 +34,8 @@ export default `.vega-embed {
 .vega-embed summary:focus {
   opacity: 1 !important;
   transition: opacity 0.2s ease;
+  outline: 2px solid black;
+  outline-offset: 2px;
 }
 .vega-embed summary:active {
   box-shadow: #aaa 0px 0px 0px 1px inset;
